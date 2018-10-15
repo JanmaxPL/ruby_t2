e8e578b1c19f28c8818e798421df850fdd91f54f
Janmax <tomix.max123@o2.pl>
2018-10-15 19:36:45 +0000

    Witaj

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "¡Hola, mundo!"
+    render html: "Witaj Świecie!"
   end
   
   def goodbye
